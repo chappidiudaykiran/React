a70484f14cd7080da53542f8e6cfb7661c0b37f9
Uday Kiran <s210540@rguktsklm.ac.in>
2025-12-14 17:03:05 +0530

    Igniting App

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", {id:"parent"},
     React.createElement("div", {id:"child"}, 
-        React.createElement("h1", {id:"heading1"}, "I am h1 tag")
+        React.createElement("h1", {id:"heading1"}, "I am h1  tag")
         )
 );
 
