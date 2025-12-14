22b21e337cb3f50bd777e398ed1d8d3333d42c4d
Uday Kiran <s210540@rguktsklm.ac.in>
2025-12-14 10:16:41 +0530

    Inception

---
+++ App.js
@@ -0,0 +1,11 @@
+
+const parent = React.createElement("div", {id:"parent"},
+    React.createElement("div", {id:"child"}, 
+        React.createElement("h1", {id:"heading1"}, "I am h1 tag")
+        )
+);
+
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
