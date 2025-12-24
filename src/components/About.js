5a071b5f83f83a9ee99b983c6cd27a10f163fa0e
Uday Kiran <s210540@rguktsklm.ac.in>
2025-12-24 22:04:40 +0530

    Class Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About=()=>{
     return(
         <div>
             <h1>About Us Page</h1>
             <p>This is the About Us page of our application.</p>
+            <User name={"Uday Kiran"} location={"Vishakapatnam"} contact={"9123456789"}/>
+            <UserClass name={"Uday Kiran"} location={"Vishakapatnam"} contact={"9123456789"}/>
         </div>
     );
 }
